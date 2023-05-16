6e78416705482927b64127044c1c23be0da85164
Nosikov Daniil <50839339+danilanosikov@users.noreply.github.com>
2023-05-16 08:44:29 +0300

    Triangle test

--- OpenGL/Source/Application.cpp
+++ OpenGL/Source/Application.cpp
@@ -1,32 +1,45 @@
+#include <GL/glew.h>
 #include <GLFW/glfw3.h>
+#include <iostream>
 
 int main(void)
 {
-    GLFWwindow* window;
-
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+   /* Initialize the library */
+    if (!glfwInit()) return -1;
 
     /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
+    GLFWwindow* pWindow = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!pWindow)
     {
         glfwTerminate();
         return -1;
     }
 
     /* Make the window's context current */
-    glfwMakeContextCurrent(window);
+    glfwMakeContextCurrent(pWindow);
+
+
+    if (glewInit() != 0) return -1;
 
     /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
+    while (!glfwWindowShouldClose(pWindow))
     {
         /* Render here */
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glBegin(GL_TRIANGLES);
+            glVertex2f(0.5f, 0.3f);
+            glVertex2f(0.4f, -0.3f);
+            glVertex2f(-0.5f, 0.6f);
+            
+            glVertex2f(0.9f, -0.1f);
+            glVertex2f(-0.5f, -0.85f);
+            glVertex2f(0.6f, -0.12f);
+        glEnd();
+
+
         /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        glfwSwapBuffers(pWindow);
 
         /* Poll for and process events */
         glfwPollEvents();
